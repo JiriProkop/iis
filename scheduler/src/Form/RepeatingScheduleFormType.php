7d9843c54bdc315bf2305cc9851513a4e6d26d68
Patrik Čerbák <cerbak01.dell@gmail.com>
2023-11-26 19:45:34 +0100

    vytvoreno pridavani aktivit

---
+++ scheduler/src/Form/RepeatingScheduleFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ScheduleWindowEntity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RepeatingScheduleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Start', TimeType::class, [
+                'input'  => 'timestamp',
+                'widget' => 'choice',
+                'mapped' => false
+            ])
+            ->add('Day', ChoiceType::class, [
+                'choices' => ["Mon" => "Mon", "Tue" => "Tue", "Wed" => "Wed", "Thu" => "Thu", "Fri" => "Fri", "Sat" => "Sat", "Sun" => "Sun"],
+                'mapped' => false
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ScheduleWindowEntity::class,
+        ]);
+    }
+}
