ece4612cd5053ce0d55e7f03e4ae5219fae5760c
Patrik Čerbák <cerbak01.dell@gmail.com>
2023-11-27 20:14:31 +0100

    class forms maji povinne udaje zaznacene

--- scheduler/src/Form/ClassFormType.php
+++ scheduler/src/Form/ClassFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\ClassEntity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,18 @@ class ClassFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Abbreviation')
-            ->add('Name')
-            ->add('Anotation')
-            ->add('Credits')
+            ->add('Abbreviation', TextType::class, [
+                'label' => 'Abbreviation *'
+            ])
+            ->add('Name', TextType::class, [
+                'label' => 'Name of the class *'
+            ])
+            ->add('Anotation', TextType::class, [
+                'label' => 'Annotation'
+            ])
+            ->add('Credits', IntegerType::class, [
+                'label' => 'Credits *'
+            ])
         ;
     }
 
